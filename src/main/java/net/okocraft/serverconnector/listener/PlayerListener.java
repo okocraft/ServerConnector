156504f3ba98e7a9f56ccb7aef2703d653feff03
Siroshun09 <siro.shun8@gmail.com>
2023-01-14 20:12:20 +0900

    refactor: move ServerConnectEvent listener from ServerListener to PlayerListener

--- src/main/java/net/okocraft/serverconnector/listener/PlayerListener.java
+++ src/main/java/net/okocraft/serverconnector/listener/PlayerListener.java
@@ -1,7 +1,10 @@
 package net.okocraft.serverconnector.listener;
 
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.kyori.adventure.translation.GlobalTranslator;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -14,6 +17,8 @@ import net.okocraft.serverconnector.util.AudienceUtil;
 import net.okocraft.serverconnector.util.FirstJoinPlayerHolder;
 
 import java.util.HashSet;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +30,43 @@ public class PlayerListener implements Listener {
 
     public PlayerListener(ServerConnectorPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onConnect(ServerConnectEvent e) {
+        var player = e.getPlayer();
+        var server = e.getTarget();
+        var serverName = server.getName();
+        var permission = server.getPermission();
+
+        // If the player do not have the permission, switch to trying to connect to the fallback server.
+        //
+        // This will only be executed if the player are still in the process of connecting to the proxy
+        // and are not connected to any server.
+        if (player.getServer() == null && !player.hasPermission(permission)) {
+            var fallbackServerName = plugin.getConfig().get(ConfigValues.SERVER_TO_SEND);
+            var fallbackServer = ProxyServer.getInstance().getServerInfo(fallbackServerName);
+
+            if (!serverName.equals(fallbackServerName) && fallbackServer != null) {
+                e.setTarget(fallbackServer);
+                serverName = fallbackServerName;
+                permission = fallbackServer.getPermission();
+            }
+        }
+
+        // Check the permissions of the server.
+        if (!player.hasPermission(permission)) {
+            var audience = AudienceUtil.player(player);
+            e.setCancelled(true);
+
+            if (player.getServer() != null) {
+                audience.sendMessage(Messages.NO_PERMISSION_TO_CONNECT_TO_SERVER.apply(serverName, permission));
+            } else {
+                var locale = Objects.requireNonNullElse(player.getLocale(), Locale.ENGLISH);
+                var translated = GlobalTranslator.render(Messages.NO_PERMISSION_TO_CONNECT_TO_PROXY, locale);
+                player.disconnect(BungeeComponentSerializer.get().serialize(translated));
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
@@ -63,18 +105,6 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler(priority = 127)
-    public void onDisconnect(PlayerDisconnectEvent e) {
-        var player = e.getPlayer();
-
-        if (joinedPlayer.contains(player.getUniqueId())) {
-            joinedPlayer.remove(e.getPlayer().getUniqueId());
-
-            var message = Messages.LEFT_PROXY.apply(player.getName());
-            AudienceUtil.all().sendMessage(message);
-        }
-    }
-
-    @EventHandler(priority = 127)
     public void onSwitch(ServerSwitchEvent e) {
         var player = e.getPlayer();
         var playerName = player.getName();
@@ -100,6 +130,18 @@ public class PlayerListener implements Listener {
         } else {
             var serverName = player.getServer().getInfo().getName();
             var message = Messages.SWITCH_SERVER.apply(playerName, serverName);
+            AudienceUtil.all().sendMessage(message);
+        }
+    }
+
+    @EventHandler(priority = 127)
+    public void onDisconnect(PlayerDisconnectEvent e) {
+        var player = e.getPlayer();
+
+        if (joinedPlayer.contains(player.getUniqueId())) {
+            joinedPlayer.remove(e.getPlayer().getUniqueId());
+
+            var message = Messages.LEFT_PROXY.apply(player.getName());
             AudienceUtil.all().sendMessage(message);
         }
     }
--- src/main/java/net/okocraft/serverconnector/listener/ServerListener.java
+++ src/main/java/net/okocraft/serverconnector/listener/ServerListener.java
@@ -1,20 +1,10 @@
 package net.okocraft.serverconnector.listener;
 
-import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
-import net.kyori.adventure.translation.GlobalTranslator;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.event.ProxyReloadEvent;
-import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.okocraft.serverconnector.ServerConnectorPlugin;
-import net.okocraft.serverconnector.config.ConfigValues;
-import net.okocraft.serverconnector.lang.Messages;
-import net.okocraft.serverconnector.util.AudienceUtil;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Locale;
-import java.util.Objects;
 
 public class ServerListener implements Listener {
 
@@ -22,43 +12,6 @@ public class ServerListener implements Listener {
 
     public ServerListener(@NotNull ServerConnectorPlugin plugin) {
         this.plugin = plugin;
-    }
-
-    @EventHandler
-    public void onConnect(ServerConnectEvent e) {
-        var player = e.getPlayer();
-        var server = e.getTarget();
-        var serverName = server.getName();
-        var permission = server.getPermission();
-
-        // If the player do not have the permission, switch to trying to connect to the fallback server.
-        //
-        // This will only be executed if the player are still in the process of connecting to the proxy
-        // and are not connected to any server.
-        if (player.getServer() == null && player.hasPermission(permission)) {
-            var fallbackServerName = plugin.getConfig().get(ConfigValues.SERVER_TO_SEND);
-            var fallbackServer = ProxyServer.getInstance().getServerInfo(fallbackServerName);
-
-            if (!serverName.equals(fallbackServerName) && fallbackServer != null) {
-                e.setTarget(fallbackServer);
-                serverName = fallbackServerName;
-                permission = fallbackServer.getPermission();
-            }
-        }
-
-        // Check the permissions of the server.
-        if (!player.hasPermission(permission)) {
-            var audience = AudienceUtil.player(player);
-            e.setCancelled(true);
-
-            if (player.getServer() != null) {
-                audience.sendMessage(Messages.NO_PERMISSION_TO_CONNECT_TO_SERVER.apply(serverName, permission));
-            } else {
-                var locale = Objects.requireNonNullElse(player.getLocale(), Locale.ENGLISH);
-                var translated = GlobalTranslator.render(Messages.NO_PERMISSION_TO_CONNECT_TO_PROXY, locale);
-                player.disconnect(BungeeComponentSerializer.get().serialize(translated));
-            }
-        }
     }
 
     @EventHandler
